853561b001abb7def4825a3addbb33a7211484df
Hiep Doan <hiep.dv179@gmail.com>
2017-03-31 12:05:58 +0200

    Initial commit

---
+++ src/main/java/com/alexthered/me/Greeting.java
@@ -0,0 +1,13 @@
+package com.alexthered.me;
+
+import lombok.Data;
+
+/**
+ * Created by hd on 31.03.17.
+ */
+@Data
+public class Greeting {
+
+    private final long id;
+    private final String content;
+}
